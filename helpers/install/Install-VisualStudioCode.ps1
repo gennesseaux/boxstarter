bada721f60075dd2ed137c8e2cd8ba8f0e48df2b
gennesseaux <jocelyn@gennesseaux.fr>
2018-09-05 21:49:58 +0200

    Changed VS Code location

--- helpers/install/Install-VisualStudioCode.ps1
+++ helpers/install/Install-VisualStudioCode.ps1
@@ -1,11 +1,13 @@
 function Install-VisualStudioCode
 {
     # install visual studio code
-    Install-ChocoApp visualstudiocode
+    Install-ChocoApp vscode -RefreshEnv -NoUpgrade
 
-    # Prevent from upgrade when using choco upgrade as it update ittself
-    chocolatey pin add -n=visualstudiocode
-    
     # Pin to task bar
-    Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Microsoft VS Code\Code.exe"
+	if (test-path("${$env:LOCALAPPDATA}\Programs\Microsoft VS Code\Code.exe")) {
+        Install-ChocolateyPinnedTaskBarItem "${env:LOCALAPPDATA}\Programs\Microsoft VS Code\Code.exe"
+    }
+    else if (test-path("${env:ProgramFiles}\Microsoft VS Code\Code.exe")) {
+        Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Microsoft VS Code\Code.exe"
+    }
 }
