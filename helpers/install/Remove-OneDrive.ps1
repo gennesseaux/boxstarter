d0a2c25396a4bf10aaef806673f6c07c432062f0
gennesseaux <jocelyn@gennesseaux.fr>
2018-09-07 18:25:48 +0200

    Remove-OneDrive rewrite

--- helpers/install/Remove-OneDrive.ps1
+++ helpers/install/Remove-OneDrive.ps1
@@ -1,38 +1,50 @@
 function Remove-OneDrive
 {
-    Write-Host "Kill OneDrive process and explorer"
-    Get-Process -Name OneDrive -ErrorAction SilentlyContinue | taskkill.exe /IM OneDrive* /F
-    Get-Process -Name explorer* -ErrorAction SilentlyContinue | taskkill.exe /IM explorer* /F
+    Write-BoxstarterMessage 'Removing OneDrive...'
 
-    Write-Host "Remove OneDrive"
-    if(Test-Path "${env:systemroot}\System32\OneDriveSetup.exe") {
-        & "${env:systemroot}\System32\OneDriveSetup.exe" /uninstall
-    }
-    if(Test-Path "${env:systemroot}\SysWOW64\OneDriveSetup.exe") {
-        & "${env:systemroot}\SysWOW64\OneDriveSetup.exe" /uninstall
-    }
+    Write-Host 'Stop OneDrive process'
+    Stop-Process -Name 'OneDrive' -Force -ErrorAction SilentlyContinue
+    Start-Sleep -s 2
 
-    Write-Host "Disable OneDrive via Group Policies"
+    Write-Host 'Uninstall OneDrive'
+    $onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+	if(!(Test-Path $onedrive)) {
+		$onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+    }
+    Start-Process $onedrive '/uninstall' -NoNewWindow -Wait
+    Start-Sleep -s 2
+
+    Write-Host 'Stop Explorer process'
+    Stop-Process -Name 'explorer' -Force -ErrorAction SilentlyContinue
+	Start-Sleep -s 2
+
+    Write-Host 'Disable OneDrive via Group Policies'
     Set-Registry -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Type 'DWord' -Value 1
 
-    Write-Host "Removing OneDrive leftovers trash"
-    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "${env:LocalAppData}\Microsoft\OneDrive"
-    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "${env:ProgramData}\Microsoft OneDrive"
-    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\OneDriveTemp"
+    Write-Host 'Removing OneDrive leftovers trash'
+    Remove-Item -Path "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue
 
-    Write-Host "Remove Onedrive from explorer sidebar"
-    New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
+    Write-Host 'Remove Onedrive from explorer sidebar'
+    if(!(Test-Path 'HKCR:')) {
+		New-PSDrive -Name 'HKCR' -PSProvider 'Registry' -Root 'HKEY_CLASSES_ROOT' | Out-Null
+	}
     Set-Registry -Path 'HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}' -Name 'System.IsPinnedToNameSpaceTree' -Type 'DWord' -Value 0
     Set-Registry -Path 'HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6' -Name 'System.IsPinnedToNameSpaceTree' -Type 'DWord' -Value 0
-    Remove-PSDrive "HKCR"
+    Remove-PSDrive 'HKCR'
 
-    Write-Host "Removing run option for new users"
-    reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
-    reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
-    reg unload "hku\Default"
+    Write-Host 'Removing run option for new users'
+    reg load 'hku\Default' 'C:\Users\Default\NTUSER.DAT'
+    reg delete 'HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' /v 'OneDriveSetup' /f
+    reg unload 'hku\Default'
 
-    Write-Host "Removing startmenu junk entry"
-    Remove-Item -Force -ErrorAction SilentlyContinue "${env:userprofile}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
+    Write-Host 'Removing startmenu junk entry'
+	Remove-Item -Path "${env:userprofile}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force -ErrorAction SilentlyContinue
 
-    Restart-Explorer
+    Write-Host 'Restart Explorer'
+    if(!(Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
+        start-Process -FilePath explorer
+    }
 }
