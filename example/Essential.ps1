58035e652408546b73f68bd12e9140e31507483d
gennesseaux <jocelyn@gennesseaux.fr>
2018-09-08 11:55:24 +0200

    More examples

--- example/Essential.ps1
+++ example/Essential.ps1
@@ -13,11 +13,33 @@ Set-ExecutionPolicy Unrestricted -Scope Process -Force
 # Profiles to install
 $profiles = @(
     'Essential'
+    'Essential-Tweaks'
 )
 
 # Define options
 $options = @(
-    'Boxstarter::WindowsUpdate=false'
+    'Boxstarter::Begin=true'
+    'Boxstarter::End=true'
+
+    'Boxstarter::Essential::Privacy-Settings=true'
+    'Boxstarter::Essential::UI-Preferences=true'
+    'Boxstarter::Essential::RemoteDesktop=true'
+    'Boxstarter::Essential::InternetExplorerESC=true'
+    'Boxstarter::Essential::TaskbarOptions=true'
+    'Boxstarter::Essential::CornerNavigationOptions=true'
+    'Boxstarter::Essential::Windows-Update=true'
+    'Boxstarter::Essential::Disable-BingSearch=true'
+    'Boxstarter::Essential::Disable-GameBarTips=true'
+    'Boxstarter::Essential::Disable-XboxGamebar=true'
+    'Boxstarter::Essential::Remove-Apps=true'
+    'Boxstarter::Essential::Remove::Microsoft.OneDrive=false'
+
+    'Boxstarter::EssentailTweaks::Privacy-tweaks=true'
+    'Boxstarter::EssentailTweaks::Security-tweaks=true'
+    'Boxstarter::EssentailTweaks::Service-tweaks=true'
+    'Boxstarter::EssentailTweaks::UI-tweaks=true'
+    'Boxstarter::EssentailTweaks::Explorer-UI-tweaks=true'
+    'Boxstarter::EssentailTweaks::Application-tweaks=true'
 )
 
 # Download my boxstarter bootstrap
