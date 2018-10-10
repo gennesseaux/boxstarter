c17c4c336f2f976d307b91e52b2abf6a90fd8fec
gennesseaux <jocelyn@gennesseaux.fr>
2018-10-10 17:48:06 +0200

    Added geforce-game-ready-driver

--- profile/Game.ps1
+++ profile/Game.ps1
@@ -2,6 +2,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #    Common tools
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if(Confirm-Install 'Boxstarter::Game::geforce')         { Install-ChocoApp geforce-experience }
-if(Confirm-Install 'Boxstarter::Game::discord')         { Install-ChocoApp discord }
-if(Confirm-Install 'Boxstarter::Game::teamspeak')       { Install-ChocoApp teamspeak }
+if(Confirm-Install 'Boxstarter::Game::geforce-experience')          { Install-ChocoApp geforce-experience }
+if(Confirm-Install 'Boxstarter::Game::geforce-game-ready-driver')   { Install-ChocoApp geforce-game-ready-driver }
+if(Confirm-Install 'Boxstarter::Game::discord')                     { Install-ChocoApp discord }
+if(Confirm-Install 'Boxstarter::Game::teamspeak')                   { Install-ChocoApp teamspeak }
