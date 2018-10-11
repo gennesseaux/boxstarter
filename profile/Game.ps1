31e77cbb50f62aecbaa6820c9b19fd7eece8a364
gennesseaux <jocelyn@gennesseaux.fr>
2018-10-11 09:48:44 +0200

    oops

--- profile/Game.ps1
+++ profile/Game.ps1
@@ -6,4 +6,4 @@ if(Confirm-Install 'Boxstarter::Game::geforce-experience')          { Install-Ch
 if(Confirm-Install 'Boxstarter::Game::geforce-game-ready-driver')   { Install-ChocoApp geforce-game-ready-driver }
 if(Confirm-Install 'Boxstarter::Game::discord')                     { Install-ChocoApp discord }
 if(Confirm-Install 'Boxstarter::Game::teamspeak')                   { Install-ChocoApp teamspeak }
-if(Confirm-Install 'Boxstarter::Game::skype')                       { Install-ChocoApp teamspeak }
+if(Confirm-Install 'Boxstarter::Game::skype')                       { Install-ChocoApp skype }
