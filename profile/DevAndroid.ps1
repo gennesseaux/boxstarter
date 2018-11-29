155a91c2344d9da62ff6c4444b4b9139a8b0477d
gennesseaux <jocelyn@gennesseaux.fr>
2018-11-29 11:48:55 +0100

    Removed jdk7

--- profile/DevAndroid.ps1
+++ profile/DevAndroid.ps1
@@ -1,7 +1,6 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #    Common tools
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if(Confirm-Install 'Boxstarter::DevAndroid::jdk7')          { Install-ChocoApp jdk7 }
 if(Confirm-Install 'Boxstarter::DevAndroid::jdk8')          { Install-ChocoApp jdk8 }
 if(Confirm-Install 'Boxstarter::DevAndroid::gradle')        { Install-ChocoApp gradle }
 if(Confirm-Install 'Boxstarter::DevAndroid::android-sdk')   { Install-ChocoApp android-sdk -RefreshEnv -NoUpgrade }
