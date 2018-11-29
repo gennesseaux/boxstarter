b232397a8ea65c6b77010fdccd785ee3ff7bbc45
gennesseaux <jocelyn@gennesseaux.fr>
2018-11-29 10:41:50 +0100

    Removed jdk7

--- profile/DevJava.ps1
+++ profile/DevJava.ps1
@@ -5,7 +5,6 @@ Import-Function -Path "$sRoot/helpers/install/Install-JetBrainsIntellijIdea.ps1"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #    Common tools
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if(Confirm-Install 'Boxstarter::DevJava::jdk7')         { Install-ChocoApp jdk7 }
 if(Confirm-Install 'Boxstarter::DevJava::jdk8')         { Install-ChocoApp jdk8 }
 if(Confirm-Install 'Boxstarter::DevJava::maven')        { Install-ChocoApp maven }
 if(Confirm-Install 'Boxstarter::DevJava::gradle')       { Install-ChocoApp gradle }
