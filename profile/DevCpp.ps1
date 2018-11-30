2d3247cda8bbbc2ca1d139e6a27def37a8269cf3
gennesseaux <jocelyn@gennesseaux.fr>
2018-11-30 10:13:20 +0100

    Added doxygen to DecCpp

--- profile/DevCpp.ps1
+++ profile/DevCpp.ps1
@@ -4,3 +4,4 @@
 
 if(Confirm-Install 'Boxstarter::DevCore::cmake')    { Install-ChocoApp cmake }
 if(Confirm-Install 'Boxstarter::DevCore::LLVM')     { Install-ChocoApp llvm }
+if(Confirm-Install 'Boxstarter::DevCore::doxygen')  { Install-ChocoApp doxygen.install }
