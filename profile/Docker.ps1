3071429f4228dd1f5b5c9c3f66f2438c87eb53ae
gennesseaux <jocelyn@gennesseaux.fr>
2019-03-03 13:32:32 +0100

    use docker-desktop instead of docker-for-windows

--- profile/Docker.ps1
+++ profile/Docker.ps1
@@ -1,6 +1,6 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #    Other common tools
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if(Confirm-Install 'Boxstarter::Docker::docker-for-windows')    { Install-ChocoApp docker-for-windows }
+if(Confirm-Install 'Boxstarter::Docker::docker-desktop')    { Install-ChocoApp docker-desktop }
 if(Confirm-Install 'Boxstarter::Docker::docker-compose')        { Install-ChocoApp docker-compose }
 if(Confirm-Install 'Boxstarter::Docker::docker-kitematic')      { Install-ChocoApp docker-kitematic }
