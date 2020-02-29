b112848f1e65743d3a0dc1e1e23311029de8d81a
gennesseaux <jocelyn@gennesseaux.fr>
2020-02-29 22:32:03 +0100

    added ninj to DevCpp

--- profile/DevCpp.ps1
+++ profile/DevCpp.ps1
@@ -8,5 +8,6 @@ Import-Function -Path "$sRoot/helpers/install/Install-vcpkg.ps1"
 
 if(Confirm-Install 'Boxstarter::DevCpp::cmake')    { Install-ChocoApp cmake }
 if(Confirm-Install 'Boxstarter::DevCpp::LLVM')     { Install-ChocoApp llvm }
+if(Confirm-Install 'Boxstarter::DevCpp::LLVM')     { Install-ChocoApp ninja }
 if(Confirm-Install 'Boxstarter::DevCpp::doxygen')  { Install-ChocoApp doxygen.install }
 if(Confirm-Install 'Boxstarter::DevCpp::vcpkg')    { Install-vcpkg c:\tools\vcpkg }
