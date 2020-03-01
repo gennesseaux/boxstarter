38e752ab11fab4169171fd095ada0186415db42a
gennesseaux <jocelyn@gennesseaux.fr>
2020-03-02 00:01:03 +0100

    back to lxrunoffline

--- profile/WSL.ps1
+++ profile/WSL.ps1
@@ -57,19 +57,27 @@ if(Confirm-Install 'Boxstarter::WSL') {
     Install-ChocoWindowsFeature Microsoft-Hyper-V-All
 
     #--- Enable Windows Subsystem for Linux
-    Install-ChocoApp wsl
+    Install-ChocoWindowsFeature Microsoft-Windows-Subsystem-Linux
 
     #--- Install lxrunoffline
     Install-ChocoApp lxrunoffline -RefreshEnv
     $tools_path = Get-ToolsLocation
     $lxrun_path = join-path $tools_path 'lxrunoffline'
-    if(Test-Path $lxrun_path) {
+    if(!(Test-Path $lxrun_path)) {
         Add-Path $lxrun_path
         Update-SessionEnvironment
     }
 
+    #--- Download Ubuntu image
+    Get-WebFile -url 'https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-amd64-root.tar.gz' -fileName "$env:TMP\ubuntu.tar.gz"
+
     #--- Install Ubuntu 18.04
-    Install-ChocoApp wsl-ubuntu-1804
+    $wsl_path = join-path $tools_path 'ubuntu'
+    lxrunoffline install -n ubuntu -d "$wsl_path" -f "$env:TMP\ubuntu.tar.gz" -s
+    if(!(Test-Path $wsl_path)) {
+        Add-Path $wsl_path
+        Update-SessionEnvironment
+    }
 
     #--- Decent bash/WSL terminal - wsltty
     Install-ChocoApp wsltty
@@ -87,37 +95,33 @@ if(Confirm-Install 'Boxstarter::WSL') {
     Get-WebFile -url 'https://raw.githubusercontent.com/treffynnon/Windows-Boxstarter-with-WSL-Ubuntu/master/config_files/wsltty/paraiso_dark.mintty' -fileName "$env:APPDATA\wsltty\themes"
     Get-WebFile -url 'https://raw.githubusercontent.com/treffynnon/Windows-Boxstarter-with-WSL-Ubuntu/master/config_files/wsltty/config' -fileName "$env:APPDATA\wsltty\config"
 
-    # Ubuntu update
-    if((wsl awk '/^ID=/' /etc/*-release | wsl awk -F'=' '{ print tolower(\$2) }') -eq 'ubuntu') {
+    # Update wsl proxy
+    if( ! ([string]::IsNullOrEmpty($proxyLocation) -and [string]::IsNullOrEmpty($proxyUser)  -and [string]::IsNullOrEmpty($proxyPassword) ) ) {
+        $proxyport = $proxyLocation.Substring($proxyLocation.LastIndexOf('/')+1)
+        $proxy = $proxyport.Substring(0, $proxyport.LastIndexOf(':'))
+        $port = $proxyport.Substring($proxyport.LastIndexOf(':')+1)
+        $user = $proxyUser.Substring($proxyUser.LastIndexOf('\')+1)
+        $http_proxy = "http://${user}:${proxyPassword}@${proxy}:${port}"
 
-        # Update wsl proxy
-        if( ! ([string]::IsNullOrEmpty($proxyLocation) -and [string]::IsNullOrEmpty($proxyUser)  -and [string]::IsNullOrEmpty($proxyPassword) ) ) {
-            $proxyport = $proxyLocation.Substring($proxyLocation.LastIndexOf('/')+1)
-            $proxy = $proxyport.Substring(0, $proxyport.LastIndexOf(':'))
-            $port = $proxyport.Substring($proxyport.LastIndexOf(':')+1)
-            $user = $proxyUser.Substring($proxyUser.LastIndexOf('\')+1)
-            $http_proxy = "http://${user}:${proxyPassword}@${proxy}:${port}"
-
-            wsl sudo bash -c "echo export http_proxy=$http_proxy >> /etc/environment"
-            wsl sudo bash -c "echo export https_proxy=$http_proxy >> /etc/environment"
-            wsl sudo bash -c "echo export ftp_proxy=$http_proxy >> /etc/environment"
-        }
-
-        # Update ubuntu
-        Start-Process "wsl" -argumentlist "sudo test -r /etc/environment && source /etc/environment" -wait
-
-        Start-Process "wsl" -argumentlist "sudo apt-get update -y && apt-get upgrade -y" -wait
-        Start-Process "wsl" -argumentlist "sudo apt-get autoremove -y && apt-get autoclean -y" -wait
-
-        Start-Process "wsl" -argumentlist "sudo apt-get install -y build-essential bzip2 cmake colordiff coreutils curl dos2unix doxygen git git-lfs linux-tools-common llvm rsync unzip wget" -wait
-        Start-Process "wsl" -argumentlist "sudo curl -L --output /tmp/nord-dircolors.zip https://github.com/arcticicestudio/nord-dircolors/archive/develop.zip && mkdir -p ~/.dircolors && unzip -o -d ~/.dircolors /tmp/nord-dircolors.zip" -wait
-
-        Start-Process "wsl" -argumentlist "sudo mkdir -p ~/.bashrc.d && chmod 700 ~/.bashrc.d" -wait
-        Start-Process "wsl" -argumentlist "sudo grep ~/.bashrc.d ~/.bashrc ||  echo >> ~/.bashrc" -wait
-        Start-Process "wsl" -argumentlist "sudo grep ~/.bashrc.d ~/.bashrc ||  echo 'for file in ~/.bashrc.d/*.bashrc; do source '\\$'{file}; done' >> ~/.bashrc" -wait
-        Start-Process "wsl" -argumentlist "sudo echo 'test -r ~/.dir_colors && eval '\\$'(dircolors ~/.dir_colors)' > ~/.bashrc.d/dir_colors.bashrc" -wait
-        Start-Process "wsl" -argumentlist "sudo chmod +x ~/.bashrc.d/*" -wait
+        wsl sudo bash -c "echo export http_proxy=$http_proxy >> /etc/environment"
+        wsl sudo bash -c "echo export https_proxy=$http_proxy >> /etc/environment"
+        wsl sudo bash -c "echo export ftp_proxy=$http_proxy >> /etc/environment"
     }
+
+    # Update ubuntu
+    wsl bash -c "test -r /etc/environment && source /etc/environment"
+
+    wsl bash -c "apt-get update -y && apt-get upgrade -y"
+    wsl bash -c "apt-get autoremove -y && apt-get autoclean -y"
+
+    wsl bash -c "apt-get install -y build-essential bzip2 cmake colordiff coreutils curl dos2unix doxygen git git-lfs linux-tools-common llvm rsync unzip wget"
+    wsl bash -c "curl -L --output /tmp/nord-dircolors.zip https://github.com/arcticicestudio/nord-dircolors/archive/develop.zip && mkdir -p ~/.dircolors && unzip -o -d ~/.dircolors /tmp/nord-dircolors.zip"
+
+    wsl bash -c "mkdir -p ~/.bashrc.d && chmod 700 ~/.bashrc.d"
+    wsl bash -c "grep ~/.bashrc.d ~/.bashrc ||  echo >> ~/.bashrc"
+    wsl bash -c "grep ~/.bashrc.d ~/.bashrc ||  echo 'for file in ~/.bashrc.d/*.bashrc; do source '\\$'{file}; done' >> ~/.bashrc"
+    wsl bash -c "echo 'test -r ~/.dir_colors && eval '\\$'(dircolors ~/.dir_colors)' > ~/.bashrc.d/dir_colors.bashrc"
+    wsl bash -c "chmod +x ~/.bashrc.d/*"
 
     #
     Update-SessionEnvironment
@@ -148,38 +152,38 @@ if(Confirm-Install 'Boxstarter::WSL-docker') {
     }
 
     # Bring Ubuntu's apt-get up to date
-    Start-Process "wsl" -argumentlist "sudo apt-get update -y" -wait
-    Start-Process "wsl" -argumentlist "sudo apt-get upgrade -y" -wait
+    wsl bash -c "apt-get update -y && apt-get upgrade -y"
+    wsl bash -c "apt-get autoremove -y && apt-get autoclean -y"
 
     # Install Docker's package dependencies.
-    Start-Process "wsl" -argumentlist "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common" -wait
+    wsl bash -c "apt-get install -y apt-transport-https ca-certificates curl software-properties-common"
 
     # Download and add Docker's official public PGP key.
-    Start-Process "wsl" -argumentlist "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -" -wait
+    wsl bash -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -"
 
     # Verify the fingerprint.
-    Start-Process "wsl" -argumentlist "sudo apt-key fingerprint 0EBFCD88" -wait
+    wsl bash -c "apt-key fingerprint 0EBFCD88"
 
     # Add the `stable` channel's Docker upstream repository.
-    Start-Process "wsl" -argumentlist 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"' -wait
+    wsl bash -c "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'"
 
     # Update the apt package list (for the new apt repo).
-    Start-Process "wsl" -argumentlist "sudo apt-get update -y" -wait
+    wsl bash -c "apt-get update -y"
 
     # Install the latest version of Docker and docker compose.
-    Start-Process "wsl" -argumentlist "sudo apt-get install -y docker.io docker-compose" -wait
+    wsl bash -c "apt-get install -y docker.io docker-compose"
 
     # Connect to a remote Docker daemon
-    wsl sudo bash -c "echo export DOCKER_HOST=tcp://localhost:2375 >> ~/.bashrc && source ~/.bashrc"
+    wsl bash -c "echo export DOCKER_HOST=tcp://localhost:2375 > ~/.bashrc.d/docker.bashrc && source ~/.bashrc.d/docker.bashrc && chmod +x ~/.bashrc.d/docker.bashrc"
 
     # Verify Everything Works
-    Start-Process "wsl" -argumentlist "docker info" -wait
-    Start-Process "wsl" -argumentlist "docker-compose --version" -wait
+    wsl bash -c "docker info"
+    wsl bash -c "docker-compose --version"
 
     # Ensure Volume Mounts Work
-    wsl sudo bash -c "echo [automount] > /etc/wsl.conf"
-    wsl sudo bash -c "echo root = / >> /etc/wsl.conf"
-    wsl sudo bash -c "echo options = ""metadata"" >> /etc/wsl.conf"
+    wsl bash -c "echo [automount] > /etc/wsl.conf"
+    wsl bash -c "echo root = / >> /etc/wsl.conf"
+    wsl bash -c "echo options = ""metadata"" >> /etc/wsl.conf"
 
     #
     if(Test-PendingReboot) { Invoke-Reboot }
